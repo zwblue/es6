4c4749479b3848f1fd43666e911b8520bf613b34
zw <18755489161@163.com>
2018-01-05 16:54:08 +0800

    对es6进行了一些练习

--- dist/index.js
+++ dist/index.js
@@ -1,23 +1,275 @@
 'use strict';
 
-var a = 1;
-console.log(a);
-var b = 3;
-console.log(b);
-var name = 'zw';
-console.log(name);
-var c = 2;
-{
-   var _c = 3;
-}
-console.log(c);
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
-for (var i = 0; i < 10; i++) {
-   console.log('循环体中:' + i);
-}
-console.log('循环体外:' + i);
+1;
+var a1 = '技术胖',
+    _undefined = undefined,
+    b1 = _undefined === undefined ? 'jspang' : _undefined;
 
-for (var _i = 0; _i < 10; _i++) {
-   console.log('循环体中:' + _i);
+console.log(a1 + b1);
+
+2;
+var a = '技术胖',
+    _ref = null,
+    b = _ref === undefined ? 'jspang' : _ref;
+
+console.log(a + b);
+
+3;
+var _foo$bar = { foo: 'JSPang', bar: '技术胖' },
+    foo = _foo$bar.foo,
+    bar = _foo$bar.bar;
+
+console.log(foo, bar);
+
+4;
+var foo1 = void 0;
+//会报错要加（）;
+var _foo = { foo1: '123456' };
+foo1 = _foo.foo1;
+console.log(foo1);
+
+5;
+var a2 = { home: 12345 },
+    b2 = { home: 23444 },
+    c2 = { home: 54443 },
+    d2 = { home: 43231 },
+    e2 = { home: 989 },
+    f2 = { home: 87382 };
+// const [a2,b2,c2,d2,e2,f2]='232332';
+
+console.log(a2, b2, c2, d2, e2, f2);
+
+6;
+function jspang() {
+    console.log(arguments.length <= 0 ? undefined : arguments[0]);
+    console.log(arguments.length <= 1 ? undefined : arguments[1]);
+    console.log(arguments.length <= 2 ? undefined : arguments[2]);
+    // 就算方法中引用多了也不会报错
 }
-console.log('循环体外:' + i);
+jspang(1, 2);
+
+7;
+var arr1 = ['www', 'jspang', 'com'];
+//let arr2=arr1;
+// let arr2=arr1;//也会改变原有的值
+var arr2 = [].concat(arr1); //不会改变原有的值，数组可以，但对象好像不行
+console.log(arr2);
+arr2.push('shengHongYu');
+console.log(arr2);
+console.log(arr1);
+
+8;
+// 查找是否存在:
+var jspang1 = '技术胖';
+var blog = '非常高兴你能看到这篇文章，我是你的老朋友技术胖。这节课我们学习字符串模版。';
+console.log(blog.includes(jspang1));
+console.log(blog.startsWith(jspang1));
+console.log(blog.endsWith(jspang1));
+
+9;
+// 重复字符串
+console.log('jspang|'.repeat(3));
+
+10;
+// 进制转换
+console.log(21);
+console.log(438);
+
+11;
+// 数值型判断和转换
+var a3 = 11 / 4;
+console.log(Number.isFinite(a3)); //true
+console.log(Number.isFinite('jspang')); //false
+console.log(Number.isFinite(NaN)); //false
+console.log(Number.isFinite(undefined)); //false
+// 是否是整数
+var a4 = 123.1;
+console.log(Number.isInteger(a4)); //false
+console.log(Number.isInteger(123)); //true
+// 整数与浮点数转换
+var a5 = '9.18';
+console.log(Number.parseInt(a5));
+console.log(Number.parseFloat(a5));
+// 整数最大安全范围，和最小安全范围
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_SAFE_INTEGER);
+// 安全整数判断
+var a8 = Math.pow(2, 53) - 1;
+console.log(Number.isSafeInteger(a8)); //false
+
+
+// json转变为数组
+var json = {
+    '0': 'jspang',
+    '1': '技术胖',
+    '2': '大胖逼逼叨',
+    length: 3
+};
+console.log(json);
+console.log(Array.from(json));
+
+var arr3 = Array.of(5, 6, 7, '技术胖', true);
+console.log(arr3);
+
+// 数组相关方法
+// find:找到一个就退出
+var arr4 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(arr4.find(function (val) {
+    return val > 5;
+}));
+
+// fill:对数组进行填充
+var arr5 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+arr5.fill('jspang', 3, 4);
+console.log(arr5);
+
+// for...of...对数组进行遍历
+var arr6 = ['jspang', '技术胖', '大胖逼逼叨'];
+var _iteratorNormalCompletion = true;
+var _didIteratorError = false;
+var _iteratorError = undefined;
+
+try {
+    for (var _iterator = arr6[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+        var item = _step.value;
+
+        console.log(item);
+    }
+
+    // 获取for...of... 的下标
+} catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+} finally {
+    try {
+        if (!_iteratorNormalCompletion && _iterator.return) {
+            _iterator.return();
+        }
+    } finally {
+        if (_didIteratorError) {
+            throw _iteratorError;
+        }
+    }
+}
+
+var _iteratorNormalCompletion2 = true;
+var _didIteratorError2 = false;
+var _iteratorError2 = undefined;
+
+try {
+    for (var _iterator2 = arr6.keys()[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+        var index = _step2.value;
+
+        console.log(index);
+    }
+
+    // 同时获取下标和val值
+} catch (err) {
+    _didIteratorError2 = true;
+    _iteratorError2 = err;
+} finally {
+    try {
+        if (!_iteratorNormalCompletion2 && _iterator2.return) {
+            _iterator2.return();
+        }
+    } finally {
+        if (_didIteratorError2) {
+            throw _iteratorError2;
+        }
+    }
+}
+
+var _iteratorNormalCompletion3 = true;
+var _didIteratorError3 = false;
+var _iteratorError3 = undefined;
+
+try {
+    for (var _iterator3 = arr6.entries()[Symbol.iterator](), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
+        var _step3$value = _slicedToArray(_step3.value, 2),
+            _index = _step3$value[0],
+            val = _step3$value[1];
+
+        console.log(_index + ':' + val);
+    }
+
+    // entries的用法
+} catch (err) {
+    _didIteratorError3 = true;
+    _iteratorError3 = err;
+} finally {
+    try {
+        if (!_iteratorNormalCompletion3 && _iterator3.return) {
+            _iterator3.return();
+        }
+    } finally {
+        if (_didIteratorError3) {
+            throw _iteratorError3;
+        }
+    }
+}
+
+var list = arr6.entries();
+console.log(list.next().value);
+console.log(list.next().value);
+console.log(list.next().value);
+
+// 为函数取一个默认值
+function add(a) {
+    var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
+
+    return a + b;
+}
+console.log(add(3));
+// 但不知道要给一个函数几个参数时，你可以用add.length来判断要输入多少个入参
+console.log(add.length);
+
+var json1 = {
+    a: 'jspang',
+    c: 'zw',
+    d: 'zy'
+};
+function fun1(_ref2) {
+    var a = _ref2.a,
+        _ref2$b = _ref2.b,
+        b = _ref2$b === undefined ? 'jspang' : _ref2$b,
+        c = _ref2.c,
+        d = _ref2.d;
+    //直接能拿到json1各个对象的值;不用循环遍历的
+    console.log(a, b, c, d);
+}
+fun1(json1);
+
+//直接能拿到json1数组对应的值;不用循环遍历的
+function fun(a, b, c) {
+    console.log(a, b, c);
+}
+fun.apply(undefined, arr6);
+
+// 判断对象是否包含某个key值
+var obj = {
+    a: 1,
+    b: 2
+};
+console.log('a' in obj);
+console.log('c' in obj);
+
+var arr8 = ['jspang', '技术胖'];
+// 判断数组的下标是否有值
+console.log(1 in arr8); // true
+console.log(0 in arr8); // true
+console.log(2 in arr8); // false   
+
+
+//！！！ 数组的遍历方法
+// forEach 自动省略为空的数组元素
+arr6.forEach(function (element) {
+    return element + '1';
+});
+console.log(arr6);
+// 不知道什么用处？？？
+arr6.filter(function (val) {
+    return val + '3';
+});
+console.log(arr6);
